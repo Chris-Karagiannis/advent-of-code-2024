968181102d197c2f79ff54d9d5c4e290e650251e
Chris-Karagiannis <chris.karagiannis30@outlook.com>
2024-12-09 10:06:30 +1030

    Day 5 Part 1

---
+++ Day 5/day5.py
@@ -0,0 +1,70 @@
+def read_file(filename):
+    infile = open(filename, 'r')
+    line_list = infile.readlines()
+    priority_list = []
+
+    for line in line_list:
+        if line == '\n':
+            break
+
+        priority_list.append(line.strip('\n').split('|'))
+
+
+    return priority_list
+
+def read_page_numbers(filename, start):
+    infile = open(filename, 'r')
+    line_list = infile.readlines()
+    new_list = []
+
+    for i in range(start, len(line_list)):
+        new_list.append(line_list[i].strip('\n').split(','))
+
+    return new_list
+
+priority_list = sorted(read_file('input.txt'), key=lambda priority: priority[0])
+page_numbers = read_page_numbers('input.txt', len(priority_list) + 1)
+priority = {}
+
+# Entrys to dictionary with all the numbers they have priority over in a list
+for entry in priority_list:
+    if entry[0] not in priority:
+        priority[entry[0]] = []
+    
+    priority[entry[0]].append(entry[1])
+
+# Add lines to list if priority is correct
+valid = []
+
+for line in page_numbers:
+    for i in range(1, len(line)):
+
+        # Checks if the index prior is in the list
+        if line[i - 1] in priority:
+
+            # Then checks if the current index value is not in the priority dictionary of the previous index
+            # If true, means the previous index doesn't have priority therefore not valid
+            if line[i] not in priority[line[i - 1]]:
+                break
+        
+        # If previous value is not in the priority dictionary means it has no priority therefore not valid
+        else:
+            break
+        
+        # If get to the end of the loop without breaks, means line is valid and can be add to list
+        if i == len(line) - 1:
+            valid.append(line)
+
+
+# Add up middle index values
+total = 0
+
+for line in valid:
+    line_length = len(line)
+    middle_index = (line_length) // 2
+    total += int(line[middle_index])
+
+print("Part 1:", total)
+
+
+
